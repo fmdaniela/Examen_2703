4c983e944d3b3fdc8ceae00fe9026d6be2e16b5a
fmdaniela <fulcomariadaniela@gmail.com>
2024-03-27 22:26:02 -0300

    ejercicio 1 y 2

---
+++ Ejerciciosexamen.js
@@ -0,0 +1,43 @@
+let lados = 6;
+if (lados === 0) {
+ console.log("Círculo");
+} else if (lados === 3) {
+ console.log("Triángulo");
+} else if (lados === 4) {
+ console.log("Cuadrado");
+} else if (lados === 5) {
+console.log("Pentágono");
+} else if (lados === 6) {
+console.log("Hexágono");
+} else {
+ console.log("Polígono");
+}
+
+let numlados = 0;
+if (numlados >= 0) {
+ console.log("Círculo");
+} else {
+ console.log("El número de lados no es correcto");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
